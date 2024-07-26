4bc1e1bcc827aed24d441a2f6f6184d1c3578afd
Sia Gupta <siagupta@microsoft.com>
2024-07-26 19:07:08 +0530

    added automation event for copy button

--- Sample Applications/WPFGallery/Controls/ControlExample2.xaml.cs
+++ Sample Applications/WPFGallery/Controls/ControlExample2.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Automation;
 using System.Windows.Automation.Provider;
+using System.Windows.Media.Animation;
+using System.Windows.Automation.Peers;
 
 
 namespace WPFGallery.Controls
@@ -24,6 +26,14 @@ namespace WPFGallery.Controls
     [ContentProperty(nameof(ExampleContent))]
     public partial class ControlExample2 : UserControl
     {
+
+        public ControlExample2()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+        }
+
         static ControlExample2()
         {
             CommandManager.RegisterClassCommandBinding(typeof(ControlExample2), new CommandBinding(ApplicationCommands.Copy, Copy_SourceCode));
@@ -34,14 +44,7 @@ namespace WPFGallery.Controls
             typeof(string),
             typeof(ControlExample2),
             new PropertyMetadata(null)
-        );
-
-        //public static readonly DependencyProperty NarratorTextProperty = DependencyProperty.Register(
-        //    nameof(NarratorText),
-        //    typeof(string),
-        //    typeof(ControlExample2),
-        //    new PropertyMetadata(null)
-        //);
+        );       
 
         public static readonly DependencyProperty ExampleContentProperty = DependencyProperty.Register(
             nameof(ExampleContent),
@@ -89,12 +92,6 @@ namespace WPFGallery.Controls
             get => (string)GetValue(HeaderTextProperty);
             set => SetValue(HeaderTextProperty, value);
         }
-
-        //public string? NarratorText
-        //{
-        //    get => (string)GetValue(NarratorTextProperty);
-        //    set => SetValue(NarratorTextProperty, value);
-        //}
 
         public object? ExampleContent
         {
@@ -187,49 +184,12 @@ namespace WPFGallery.Controls
 
         private void CopyButton_Click(object sender, RoutedEventArgs e)
         {
-            string textToNarrate = "Hello, this is a sample text to be narrated.";
-            AnnounceText(textToNarrate);
-        }
-
-        //private void AnnounceText(string text)
-        //{
-        //    // Create an AutomationElement for the main window
-        //    AutomationElement element = AutomationElement.FromHandle(new System.Windows.Interop.WindowInteropHelper(this).Handle);
-
-        //    // Create an AutomationEventArgs with the required Message
-        //    AutomationEventArgs args = new AutomationEventArgs(AutomationElement.AutomationFocusChangedEvent);
-        //    AutomationInteropProvider.RaiseAutomationEvent(AutomationElement.AutomationFocusChangedEvent, element, args);
-
-        //    // Set the live setting to assertive (or polite, depending on your preference)
-        //    AutomationLiveSetting liveSetting = AutomationLiveSetting.Assertive;
-        //    AutomationProperties.SetLiveSetting(element, liveSetting);
-
-        //    // Set the announcement text
-        //    AutomationProperties.SetName(element, text);
-        //}
-
-        //private void CopyButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    string textToNarrate = "Hello, this is a sample text to be narrated.";
-        //    AnnounceText(textToNarrate);
-        //}
-
-        private void AnnounceText(string text)
-        {
-            Window parentWindow = Window.GetWindow(this);
-            // Create an AutomationElement for the main window
-            //AutomationElement element = AutomationElement.FromHandle(new System.Windows.Interop.WindowInteropHelper(parentWindow).Handle);
-            var provider = AutomationInteropProvider.HostProviderFromHandle(new System.Windows.Interop.WindowInteropHelper(parentWindow).Handle);
-            // Create an AutomationEventArgs with the required Message
-            AutomationEventArgs args = new AutomationEventArgs(AutomationElement.AutomationFocusChangedEvent);
-            AutomationInteropProvider.RaiseAutomationEvent(InvokePatternIdentifiers.InvokedEvent, provider, args);
-
-            // Set the live setting to assertive (or polite, depending on your preference)
-            AutomationLiveSetting liveSetting = AutomationLiveSetting.Assertive;
-            AutomationProperties.SetLiveSetting(parentWindow, liveSetting);
-
-            // Set the announcement text
-            AutomationProperties.SetName(parentWindow, text);
+            UIElement ue = this.XamlCodeButton;
+            var peer = FrameworkElementAutomationPeer.FromElement(ue);
+            if (peer != null)
+            {
+                peer.RaiseNotificationEvent(AutomationNotificationKind.ActionCompleted, AutomationNotificationProcessing.ImportantMostRecent, "Copied to clipboard", "CopiedToClipboardActivityId");
+            }
         }
 
     }
